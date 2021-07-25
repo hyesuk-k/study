7d49d588d0095c4c29ca80af6e1ca0f4e3293555
hyesukk <hyesuk.kim612@gmail.com>
2021-07-25 23:43:35 +0900

    [bj] #15552

---
+++ 15552.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	int t;
+	int a, b;
+
+	scanf("%d", &t);
+
+	for(int i = 0 ; i < t ; i++) {
+		scanf("%d %d", &a, &b);
+
+		printf("%d\n", a+b);
+
+	}
+
+	return 0;
+}
