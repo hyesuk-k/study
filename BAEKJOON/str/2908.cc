290d15e04660db17e1f68189fdb7f81fb87e7879
hyesukk <hyesuk.kim612@gmail.com>
2021-09-30 19:55:39 +0900

    [bj] #2908

---
+++ BAEKJOON/str/2908.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  string num1, num2;
+
+  cin >> num1 >> num2;
+
+  reverse(num1.begin(), num1.end());
+  reverse(num2.begin(), num2.end());
+
+  if (stoi(num1) > stoi(num2)) {
+    cout << num1 << endl;
+  } else {
+    cout << num2 << endl;
+  }
+
+
+
+  return 0;
+}
