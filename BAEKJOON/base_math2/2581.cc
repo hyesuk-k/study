09f9329496edc193ca1644515f935052c041692b
hyesukk <hyesuk.kim612@gmail.com>
2021-11-19 22:56:19 +0900

    [bj] #2581 수정중

---
+++ BAEKJOON/base_math2/2581.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+  int m, n;
+  cin >> m >> n;
+
+  int sum = 0;
+  int min = 0;
+
+  if (m == 1) {
+    m == min == 2;
+  }
+
+  while (m <= n) {
+    
+  }
+
+
+}
