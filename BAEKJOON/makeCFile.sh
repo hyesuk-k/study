d9927543daa19318db5600b5cc05ff8d453e81ee
hyesukk <hyesuk.kim612@gmail.com>
2023-01-04 00:41:20 +0000

    백준 문제 정보 가져오는 스크립트 수정
    - 사용 방법 동일 ./makeCFile.sh [문제ID]
    - solved.ac api 사용

--- BAEKJOON/makeCFile.sh
+++ BAEKJOON/makeCFile.sh
@@ -6,11 +6,23 @@ if [[ ${#} = 0 ]]; then
 else
     CASE_NUM=${1}
     # 문제 번호 입력 시 
-    CASE_TXT=$(curl -s https://www.acmicpc.net/problem/${CASE_NUM} | sed -n "s/^.*<title>\(.*\)<\/title>.*$/\1/p" )
-
+    # CASE_TXT=$(curl -s https://www.acmicpc.net/problem/${CASE_NUM} | sed -n "s/^.*<title>\(.*\)<\/title>.*$/\1/p" )
+    #  >> 403 Forbidden
+    
+    # USE solved ac api
+    URL='https://solved.ac/api/v3/problem/show?problemId='${CASE_NUM}
+    CASE_TXT=$(curl --request GET  --url ${URL} --header 'Content-Type: application/json')
+    CASE_KO_NAME=$(echo $CASE_TXT | jq '.titleKo')
+    CASE_KEY=$(echo $CASE_TXT | jq '.tags[0].key' | sed 's/\"//g')
     FILE_NAME=${CASE_NUM}.cc
 
-    echo "// ${CASE_TXT}" >> ${FILE_NAME}
+    echo "// ${CASE_KO_NAME}" >> ${FILE_NAME}
+    echo "// CATEGORY : ${CASE_KEY}" >> ${FILE_NAME}
+
+    if [ ! -e $CASE_KEY ];then
+        mkdir $CASE_KEY
+    fi
+    mv ${FILE_NAME} $CASE_KEY
 
     echo "Done. make ${FILE_NAME}"
 fi
